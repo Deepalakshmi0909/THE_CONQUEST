c1a8f05c25e6ad96a5712d53c93b972ffafd304b
Deepalakshmi0909 <chidambaramdeepa66@gmail.com>
2026-01-27 13:50:02 +0530

    initial commit

--- src/Main3.java
+++ src/Main3.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class Main3 {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+
+        int a=input.nextInt();
+        int b=input.nextInt();
+
+        if(a>b){
+            System.out.println("a big");
+        }
+        else{
+            System.out.println("b big");
+        }
+
+    }
 
 }
