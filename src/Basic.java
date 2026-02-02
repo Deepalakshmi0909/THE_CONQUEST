f541a1cc0bc198d1d217f225fc19437ef79e62ee
Deepalakshmi0909 <chidambaramdeepa66@gmail.com>
2026-02-02 11:44:36 +0530

    initial

---
+++ src/Basic.java
@@ -0,0 +1,30 @@
+public class pattern3 {
+    public static void main(String[]args){
+        //Basic 1
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+
+
+     /*int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+    }
+}
---
+++ src/Java_Class.java
@@ -0,0 +1,16 @@
+public class Main7 {
+    //Attributes
+    String fname="John";
+    int n=10;
+    public static void main(String []args){
+        Main7 obj=new Main7();
+        obj.fname="geeya";
+        System.out.println(obj.fname);
+        System.out.println(obj.n);
+    }
+
+}// Accessing attributes by creating an object of the class and by using . syntax ( . )
+// Acessing attribute
+//Modify attributes
+//declare the attributes as final.
+//
